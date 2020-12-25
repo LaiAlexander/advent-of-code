cd0cdcb0bc4b9aa06e4091d8b1cf48808691bb77
Lai Alexander <laialexanderholmsen@gmail.com>
2020-12-25 20:14:39 +0100

    Clean up code in day 2

--- 2020/day02/day02.py
+++ 2020/day02/day02.py
@@ -1,34 +1,35 @@
-def part1(file):
-    valid_passwords = 0
-    for line in file.readlines():
-        policy, password = line.split(": ")
-        password = password.replace("\n", "")
-        minimum, maximum = policy.split("-")
-        maximum, letter = maximum.split(" ")
-        minimum = int(minimum)
-        maximum = int(maximum)
-        lettercount = password.count(letter)
-        if minimum <= lettercount <= maximum:
-            valid_passwords += 1
-    print(valid_passwords)
+def validate_part1(minimum, maximum, letter, password):
+    lettercount = password.count(letter)
+    if minimum <= lettercount <= maximum:
+        return True
+    return False
 
-def part2(file):
-    valid_passwords = 0
+
+def validate_part2(first_pos, second_pos, letter, password):
+    first_pos -= 1
+    second_pos -= 1
+    if bool(password[first_pos] == letter) ^ bool(password[second_pos] == letter):
+        return True
+    return False
+
+with open("input.txt", "r") as file:
+    valid_passwords_part1 = 0
+    valid_passwords_part2 = 0
+
     for line in file.readlines():
         policy, password = line.split(": ")
         password = password.translate({ord("\n"): None})
-        first_pos, second_pos = policy.split("-")
-        second_pos, letter = second_pos.split(" ")
-        first_pos = int(first_pos)
-        second_pos = int(second_pos)
-        first_pos -= 1
-        second_pos -= 1
-        if bool(password[first_pos] == letter) ^ bool(password[second_pos] == letter):
-            valid_passwords += 1
-    print(valid_passwords)
 
-with open("input.txt", "r") as file:
-    part1(file)
+        first_num, second_num = policy.split("-")
+        second_num, letter = second_num.split(" ")
 
-with open("input.txt", "r") as file:
-    part2(file)
+        first_num = int(first_num)
+        second_num = int(second_num)
+
+        if validate_part1(first_num, second_num, letter, password):
+            valid_passwords_part1 += 1
+        if validate_part2(first_num, second_num, letter, password):
+            valid_passwords_part2 += 1
+
+    print(f"Valid passwords part 1: {valid_passwords_part1}")
+    print(f"Valid passwords part 2: {valid_passwords_part2}")
